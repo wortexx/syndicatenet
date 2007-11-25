43912e0fff7f5d93c77d25ad7ffffa669b829985
rootnod3 <rootnod3@b8b74795-c93f-0410-ba83-8f11c92a64d7>
2007-11-25 23:58:19 +0000

    first import

---
+++ trunk/syndicatenet/SyndicateNet/Elements/IDDistributor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SyndicateNet.Elements
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    class IDDistributor
+    {
+        #region Private Members
+        /// <summary>
+        /// 
+        /// </summary>
+        private List<long> recycled
+            = new List<long>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private long firstFreeID = 0;
+        #endregion
+
+        #region Accessors
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>The new ID</returns>
+        public long NewID
+        {
+            get
+            {
+                long newID;
+                if (recycled.Count == 0)
+                    newID = firstFreeID++;
+                else
+                {
+                    newID = recycled[0];
+                    recycled.RemoveAt(0);
+                }
+
+                return newID;
+            }
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <param name="id">
+        ///     The ID to remove
+        /// </param>
+        public void FreeID(long id)
+        {
+            if (id >= firstFreeID)
+            {
+                // Exception
+            }
+
+            recycled.Add(id);
+        }
+        #endregion
+    }
+}
