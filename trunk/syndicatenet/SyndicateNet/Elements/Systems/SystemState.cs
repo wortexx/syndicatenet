6860a1311abcced8d8b296a32c71a8abdf2f3c86
rootnod3 <rootnod3@b8b74795-c93f-0410-ba83-8f11c92a64d7>
2007-11-26 00:29:59 +0000

    - added MIT License header to all files

--- trunk/syndicatenet/SyndicateNet/Elements/Systems/SystemState.cs
+++ trunk/syndicatenet/SyndicateNet/Elements/Systems/SystemState.cs
@@ -1,4 +1,29 @@
-﻿using System;
+﻿/// <license>
+/// Copyright (c) 2007-2008 Simon Wollwage
+/// 
+/// Permission is hereby granted, free of charge, to any person
+/// obtaining a copy of this software and associated documentation
+/// files (the "Software"), to deal in the Software without
+/// restriction, including without limitation the rights to use,
+/// copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following
+/// conditions:
+/// 
+/// The above copyright notice and this permission notice shall be
+/// included in all copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+/// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+/// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+/// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+/// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+/// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+/// OTHER DEALINGS IN THE SOFTWARE.
+/// </license>
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +36,7 @@ namespace SyndicateNet.Elements.Systems
     [Serializable]
     class SystemState
     {
+        #region Protected Members
         /// <summary>
         /// 
         /// </summary>
@@ -20,7 +46,9 @@ namespace SyndicateNet.Elements.Systems
         /// 
         /// </summary>
         protected double relatedSystemVersion = 0.0;
+        #endregion
 
+        #region Constructors
         /// <summary>
         /// 
         /// </summary>
@@ -31,7 +59,9 @@ namespace SyndicateNet.Elements.Systems
             this.relatedSystem = relatedSystem;
             this.relatedSystemVersion = relatedSystem.SystemVersion;
         }
+        #endregion
 
+        #region Public Methods
         /// <summary>
         /// 
         /// </summary>
@@ -44,5 +74,6 @@ namespace SyndicateNet.Elements.Systems
             return ((relatedSystemVersion == relatedSystem.SystemVersion) &&
                 (this.relatedSystem == relatedSystem));
         }
+        #endregion
     }
 }
